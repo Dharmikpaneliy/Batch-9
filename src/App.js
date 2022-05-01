25f7ee4e22ff89760b6105b97bcc83405a2cee7d
Dharmikpaneliy <dhramikpaneliya7@gmail.com>
2022-05-01 17:24:53 +0530

    git_setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(props) {
+    let arrdata = 
+    [
+        {
+            id: 101,
+            name: 'Abacavir',
+            quantity: 25,
+            price: 150,
+            expiry: 2022,
+            status: true
+          },
+          {
+            id: 102,
+            name: 'Eltrombopag',
+            quantity: 90,
+            price: 550,
+            expiry: 2021,
+            status: true
+          },
+          {
+            id: 103,
+            name: 'Meloxicam',
+            quantity: 85,
+            price: 450,
+            expiry: 2025,
+            status: false
+          },
+          {
+            id: 104,
+            name: 'Allopurinol',
+            quantity: 50,
+            price: 600,
+            expiry: 2023,
+            status: true
+          },
+          {
+            id: 105,
+            name: 'Phenytoin',
+            quantity: 63,
+            price: 250,
+            expiry: 2021,
+            status: false
+          }
+    ];
+    
+    console.log(arrdata.map((i) => {return i}));  //map
+
+    console.log(arrdata.filter((g) => {return g.expiry >= 2022}));  //filter
+
+    return (
+        <div>
+            {/* <h1>Hello</h1> */}
+        </div>
+    );
 }
 
 export default App;
