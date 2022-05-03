25aaf6f0a64b2e3be2b4825f1d42a480f7677ac5
Dharmikpaneliy <dhramikpaneliya7@gmail.com>
2022-05-03 15:30:11 +0530

    git_setup

--- src/App.js
+++ src/App.js
@@ -3,64 +3,66 @@ import React from 'react';
 function App(props) {
     const arrdata = 
     [
-        {
-            id: 101,
-            name: 'Abacavir',
-            quantity: 25,
-            price: 150,
-            expiry: 2022,
-            status: true
-          },
-          {
-            id: 102,
-            name: 'Eltrombopag',
-            quantity: 90,
-            price: 550,
-            expiry: 2021,
-            status: true
-          },
-          {
-            id: 103,
-            name: 'Meloxicam',
-            quantity: 85,
-            price: 450,
-            expiry: 2025,
-            status: false
-          },
-          {
-            id: 104,
-            name: 'Allopurinol',
-            quantity: 50,
-            price: 600,
-            expiry: 2023,
-            status: true
-          },
-          {
-            id: 105,
-            name: 'Phenytoin',
-            quantity: 63,
-            price: 250,
-            expiry: 2021,
-            status: false
-          }
+      {
+        name: "amit",
+        age: 35,
+        salary: 40000,
+        bonus: 1000,
+        status: true
+      },
+      {
+        name: "ajay",
+        age: 25,
+        salary: 38000,
+        bonus: 2000,
+        status: false
+      },
+      {
+        name: "mayur",
+        age: 23,
+        salary: 50000,
+        bonus: 500,
+        status: true
+      },
+      {
+        name: "jay",
+        age: 29,
+        salary: 35000,
+        bonus: 600,
+        status: true
+      },
+      {
+        name: "raj",
+        age: 33,
+        salary: 22000,
+        bonus: 2000,
+        status: true
+      }
     ];
     
-    arrdata.map((value,index) => console.log(value.id,value.name,value.quantity,value.price,value.expiry,value.status));
+    arrdata.map((value,index) => console.log(value.name,value.age,value.salary,value.bonus,value.status));
 
     let ans = 
-            arrdata.filter((d,i) => d.expiry >= 2022)
-            .reduce((acc,d,i) => acc + d.price,0);
+            arrdata.filter((d,i) => d.status == true)
+            .reduce((bou,d,i) => bou + d.salary,0);
+
+    let obj = {
+               id : 100 , 
+               name : "Dharmik"
+              };
+    let {id,name} = obj;
+    console.log(id,name);
 
 
     return (
       <>
         <table border="1">
           <tr>
-            <td>Id</td>
             <td>Name</td>
-            <td>Quantity</td>
-            <td>price</td>
-            <td>expiry</td>
+            <td>Age</td>
+            <td>Salary</td>
+            <td>Bouns</td>
+            <td>Status</td>
             <td>Total</td>
           </tr>
           {
@@ -68,11 +70,11 @@ function App(props) {
             {
               return(
                 <tr>
-                  <td>{value.id}</td>
                   <td>{value.name}</td>
-                  <td>{value.quantity}</td>
-                  <td>{value.price}</td>
-                  <td>{value.expiry}</td>
+                  <td>{value.age}</td>
+                  <td>{value.salary}</td>
+                  <td>{value.bouns}</td>
+                  <td>{value.status}</td>
                   {index === 0?<td rowspan={arrdata.length}>{ans}</td>:null}
                 </tr>
               )
