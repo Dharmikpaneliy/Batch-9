981bebe7bf4e196d7fd61512f7a459b7719e3341
Dharmikpaneliy <dhramikpaneliya7@gmail.com>
2022-05-04 13:37:53 +0530

    Git_setup

--- src/App.js
+++ src/App.js
@@ -1,88 +1,119 @@
 import React from 'react';
 
 function App(props) {
-    const arrdata = 
-    [
-      {
-        name: "amit",
-        age: 35,
-        salary: 40000,
-        bonus: 1000,
-        status: true
-      },
-      {
-        name: "ajay",
-        age: 25,
-        salary: 38000,
-        bonus: 2000,
-        status: false
-      },
-      {
-        name: "mayur",
-        age: 23,
-        salary: 50000,
-        bonus: 500,
-        status: true
-      },
-      {
-        name: "jay",
-        age: 29,
-        salary: 35000,
-        bonus: 600,
-        status: true
-      },
-      {
-        name: "raj",
-        age: 33,
-        salary: 22000,
-        bonus: 2000,
-        status: true
-      }
-    ];
-    
-    arrdata.map((value,index) => console.log(value.name,value.age,value.salary,value.bonus,value.status));
+  
+const emp =[
+  {
+    name: 'amit',
+    age: 35,
+    salary: 40000,
+    bonus:1000,
+    status: true
+  },
+  {
+    name: 'ajay',
+    age: 25,
+    salary: 38000,
+    bonus: 2000,
+    status: false
+  },
+  {
+    name: 'mayur',
+    age: 23,
+    salary: 50000,
+    bonus: 500,
+    status: true
+  },
+  {
+    name: 'jay',
+    age: 29,
+    salary: 35000,
+    bonus: 600,
+    status: true
+  },
+  {
+    name: 'raj',
+    age: 33,
+    salary: 22000,
+    bonus: 2000,
+    status: true
+  }
+];
 
-    let ans = 
-            arrdata.filter((d,i) => d.status == true)
-            .reduce((bou,d,i) => bou + d.salary,0);
+// destructure
 
-    let obj = {
-               id : 100 , 
-               name : "Dharmik"
-              };
-    let {id,name} = obj;
-    console.log(id,name);
+let[name]=emp;
+console.log(name);
+
+console.log(emp.map((i) => {return i})); 
 
 
+let em = emp.filter((e) => {return e.status >= true});
+console.log(em);
+
+let emans = em.reduce((acc, emp, i) => acc + emp.bonus + emp.salary, 0);
+console.log(emans);
+
+let eman = emp.reduce((acc, emp, i) => acc + emp.bonus + emp.salary, 0);
+console.log(eman);
+     
     return (
-      <>
-        <table border="1">
-          <tr>
-            <td>Name</td>
-            <td>Age</td>
-            <td>Salary</td>
-            <td>Bouns</td>
-            <td>Status</td>
-            <td>Total</td>
-          </tr>
+    <>
+        <h4>Employee Data</h4>
+      <table border = "1">
+        <tr>
+        <th>Name</th>
+        <th>Age</th>
+        <th>Salary</th>
+        <th>Bonus</th>
+        <th>Total</th>
+        </tr>
+        <tbody>
           {
-            arrdata.map((value,index) => 
-            {
+            emp.map((emp,index) => {
               return(
                 <tr>
-                  <td>{value.name}</td>
-                  <td>{value.age}</td>
-                  <td>{value.salary}</td>
-                  <td>{value.bouns}</td>
-                  <td>{value.status}</td>
-                  {index === 0?<td rowspan={arrdata.length}>{ans}</td>:null}
+                  <td>{emp.name}</td>
+                  <td>{emp.age}</td>
+                  <td>{emp.salary}</td>
+                  <td>{emp.bonus}</td>
+                  {index === 0 ? <td rowspan = {emp.length = "5"} > {eman} </td> : null} 
                 </tr>
               )
             })
           }
-        </table>
-      </>
-    );
-}
+        </tbody>
+      </table>
 
+    <br></br>
+        <h4>Employee Data-1</h4>
+    <table border = "1">
+        <tr>
+        <th>Name</th>
+        <th>Age</th>
+        <th>Salary</th>
+        <th>Bonus</th>
+        <th>Total</th>
+        </tr>
+        <tbody>
+          {
+            em.map((emp,index) => {
+              return(
+                <tr>
+                  <td>{emp.name}</td>
+                  <td>{emp.age}</td>
+                  <td>{emp.salary}</td>
+                  <td>{emp.bonus}</td>
+                  {index === 0 ? <td rowspan = {emp.length = "5"} > {emans} </td> : null} 
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
+    </>
+  );
+};
+       
+  
 export default App;
