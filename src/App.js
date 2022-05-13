7c53497b7f0d178d3ca0952106f2f6838d66103b
Dharmikpaneliy <dhramikpaneliya7@gmail.com>
2022-05-13 14:46:01 +0530

    git_setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Loading from './components/Loading/Loading';
 import Branch from './containers/country/Branch';
 import Cityfun from './containers/country/Cityfun';
 import CountryFun from './containers/country/countryFun';
+import Home from './containers/Home/Home';
 import Time from './DT/Time';
 import Timefun from './DT/Timefun';
 
+const HomeWithLoading = Loading(Home)
+
 function App(props) {
-  
+  const[loading,setLoading] = useState(false);
+  const[data,setData] = useState([]);
 
-    return (
-    <>
-      
-    {/* <CountryFun />
-    <Cityfun />
-    <Branch /> */}
+  let orgData = [
+    {id: 101, name:"Amit"},
+    {id: 102, name:"Kevin"},
+    {id: 103, name:"Piyush"}
+  ]
 
-    {/* <Time /> */}
+  useEffect(
+    () => {
+      setLoading(true);
+      setTimeout(()=>{setLoading(false); setData(orgData)},3000);
+    },
+  [])
 
-    <Timefun />
+  console.log(loading,data);
 
-    </>
+  return (
+  <>
+      <HomeWithLoading
+         isLoading={loading}
+         data={data}
+      />    
+  </>
   );
 };
        
